055a47ae88e05eac9fc50f098e11cafa4e54ed7d
iconic <122547494+Anirban1704@users.noreply.github.com>
2026-09-02 02:17:04 +0000

    Here I learn about useRef. 1. useRef vs useState  a. useState is use to re-execution when it's value change.  b it is use full with values which need to render in ui  c. useRef does not cause a re-execution when it value change.  b it is usefull with those values which are not using in ui.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import Player from "./components/Player";
 
 function App() {
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import React from "react";
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+  // const [enteredPlayerName, setenteredPlayerName] = useState();
+
+  function handleSetNameClick() {
+    // setenteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>
+        Welcome{" "}
+        {playerName.current ? playerName.current.value : "unknown entity"}
+      </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleSetNameClick}>Set Name</button>
       </p>
     </section>
   );
